a5710a29d35d7bb03771de091fece2c0c9747364
atvfool <atvfool@gmail.com>
2021-04-15 22:08:43 -0400

    File I/O
    Simple file input and output

---
+++ FileIO.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ifstream inStream;
+    inStream.open("passphrase.txt");
+    ofstream outStream;
+    outStream.open("NewPassPhrase.txt", ios::app);
+
+    string passPhrase;
+    int userGuess = 0;
+
+    if(!inStream.fail()){
+        while(inStream >> passPhrase){
+            cout << "The pass phrase is: " << passPhrase << endl;
+            cout << "What is your answer?\n";
+            cin >> userGuess;
+            if(userGuess == passPhrase.length())
+                cout << "Congrats\n";
+            else{
+                cout << "Sorry try again later\n";
+                outStream << passPhrase << endl;
+            }
+                
+        }
+    }
+    inStream.close();
+    outStream.close();
+}
+
