465ff129c6c88dda5942d3f87c4b5dcc3304ecf9
atvfool <atvfool@gmail.com>
2021-04-15 21:46:20 -0400

    Adds functions and AI
    Moves a lot of the code to various functions and includes an "AI" player now that just picks a random number

--- ChipsGame.cpp
+++ ChipsGame.cpp
@@ -3,8 +3,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <ctype.h>
 
 using namespace std;
+
+// Prototypes
+string GetPlayerName(string[], bool);
+int AskMove(bool, int, string[]);
+int CalcMaxChips(int);
+void GetUserNames(string[]);
 
 const int MAX_CHIPS = 100;
 const float MAX_TURN = .5;
@@ -18,14 +25,9 @@ int main(){
     bool gameOver = false;
     int chipsInPile = 0;
     int chipsTaken = 0;
-    int maxChipsToTake = 0;
 
     // Get the players names
-    cout << "Enter player 1 name\n";
-    cin >> playerName[0];
-
-    cout << "Enter player 2 name\n";
-    cin >> playerName[1];
+    GetUserNames(playerName);
 
     srand(time(0));
 
@@ -35,22 +37,8 @@ int main(){
         cout << "This round will start with " << chipsInPile << " chips in the pile \n";
         gameOver = false;
         while(!gameOver){
-            maxChipsToTake = (chipsInPile * MAX_TURN); // get the number of chips that can be taken this round
-            
-            do{
-                if(player1Turn){
-                    cout << playerName[0] << " how many chips would you like?\n";
-                }else{
-                    cout << playerName[1] << " how many chips would you like?\n";
-                }
-                cout << "You can take up to ";
-                if(maxChipsToTake == 0)
-                    cout << "1\n";
-                else
-                    cout << maxChipsToTake << endl;
-                cin >> chipsTaken;
-            }while(chipsTaken > maxChipsToTake && chipsInPile > 1);
-
+            // Get how many chips the player has taken
+            chipsTaken = AskMove(player1Turn, chipsInPile, playerName);
             chipsInPile = chipsInPile - chipsTaken;
             cout << "There are " << chipsInPile << " left in the pile\n";
 
@@ -59,17 +47,60 @@ int main(){
                 gameOver = true;
             }else{
                 player1Turn = !player1Turn; // flip whos turn it is when game isn't over
-            }
-            
+            }   
         }
-        if(player1Turn)
-            cout << "Congrats " << playerName[1] << " on winning!\n";
-        else
-            cout << "Congrats " << playerName[0] << " on winning!\n";
+        
+        cout << "Congrats " << GetPlayerName(playerName, !player1Turn) << " on winning!\n";
 
         cout << "Do you want to play again?Yes(y), No(n)\n";
         cin >> playAgain;
-    }while (playAgain == 'y' || playAgain == 'Y');
+    }while (tolower(playAgain) == 'y');
     
     return 0;
+}
+
+string GetPlayerName(string names[], bool playerTurn){
+    string name;
+    
+    if(playerTurn)
+        name = names[0];
+    else
+        name = names[1];
+    
+    return name;
+}
+
+int AskMove(bool player1Turn, int chipsInPile, string names[]){
+    int chipsTaken = 0;
+    int maxChips = CalcMaxChips(chipsInPile);
+    do{
+        cout << GetPlayerName(names, player1Turn) << " how many chips would you like?\n";
+        cout << "You can take up to ";
+        if(maxChips == 0)
+            cout << "1\n";
+        else
+            cout << maxChips << endl;
+        if(GetPlayerName(names, player1Turn) == "AI"){
+            if(maxChips == 0)
+                chipsTaken = 1;
+            else
+                chipsTaken = (rand() % maxChips) + 1;
+        }else{
+            cin >> chipsTaken;
+        }
+    }while(chipsTaken > maxChips && chipsInPile > 1);
+    return chipsTaken;
+}
+
+int CalcMaxChips(int chipsInPile){
+    int maxChips = chipsInPile * MAX_TURN;
+    return maxChips;
+}
+
+void GetUserNames(string players[]){
+    cout << "Enter player 1 name\n";
+    cin >> players[0];
+
+    cout << "Enter player 2 name (If you wish to play against the computer enter the name AI)\n";
+    cin >> players[1];
 }
